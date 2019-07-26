208f114d7af9bda7c34ae93b787be6bb9bab6e86
Guilherme <gcaramori@outlook.com>
2019-07-26 14:23:02 -0300

    v3.5

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,3 @@
-
-
 var elSetorTurno = $('.setor-turno')
 var elPergunta1 = $('.perguntas1')
 var elPergunta2 = $('.perguntas2')
@@ -8,59 +6,43 @@ var elPergunta4 = $('.perguntas4')
 
 var posicao = 0
 
+console.log('teste');
+const $inputs = $('.inputs');
+$inputs.keyup(function(e) {
+    if (this.value.length === this.maxLength) {
+        $(this).nextAll('.inputs').first().focus();
+    }
+});
+
 $('.txtSetor').focus()
 
-$('.btnNextPage:eq(0)').click(function () {
-    if (posicao == 0) {
-        $(elSetorTurno).addClass('hide')
-        $('.txtPergunta1').focus()
-        posicao++;
-    }
-})
+$('.btnNextPage').click(function() {
+    const $parent = $(this).parent();
+    $parent.addClass('hide').removeClass('show');
+    $parent.next().addClass('show').removeClass('hide');
+    $parent.next().find('input').first().focus();
+});
 
-$('.btnNextPage:eq(1)').click(function () {
-    if (posicao == 1) {
-        jQuery(elPergunta1).addClass('hide')
-        $('.txtPergunta4').focus()
-        posicao++;
-    }
-})
-
-$('.btnNextPage:eq(2)').click(function () {
-    if (posicao == 2) {
-        jQuery(elPergunta2).addClass('hide')
-        $('.txtPergunta7').focus()
-        posicao++;
-    }
-})
-
-$('.btnNextPage:eq(3)').click(function () {
-    if(posicao == 3) {
-        jQuery(elPergunta3).addClass('hide')
-        $('.txtObs').focus()
-        posicao++;
-    }
-})
-
-$('.btnNextPage:eq(4)').click(function () {
-    if (posicao == 4) {
-        alert('Avaliação cadastrada!')
-        $('#formCadastra').submit()
-        limpar()
-        $('.txtSetor').focus()
-        jQuery(elSetorTurno).removeClass('hide')
-        posicao = 0;
-    }
-})
+$('.btnNextPage:not(#finalBtn)').click(function() {
+    const $parent = $(this).parent();
+    $parent.addClass('hide').removeClass('show');
+    $parent.next().addClass('show').removeClass('hide');
+    $parent.next().find('input').first().focus();
+});
+$('#finalBtn').click(function() {
+    alert('Avaliação cadastrada!');
+    $('#formCadastra').submit();
+    limpar();
+});
 
 $('.btnCadastro').click(function() {
+    $('#cadastro').addClass('show')
     $('#listagem').removeClass('show')
-    $('#cadastro').removeClass('hide')
 })
 
 $('.btnListar').click(function() {
     $('#listagem').addClass('show')
-    $('#cadastro').addClass('hide')
+    $('#cadastro').removeClass('show')
 })
 
 function limpar() {
